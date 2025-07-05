2e0b22c0df6fb6444252cca8712bc629bb152b7c
Harshil Bambhroliya <harshilbambhroliya048@gmail.com>
2025-07-05 10:42:41 +0530

    Change default server port from 5000 to 8080

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const fs = require("fs");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 // Enable CORS with specific origins
 app.use(
